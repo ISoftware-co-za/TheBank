11d335198c9bd451a66bd6f82a0110280fc57e52
Keith Crompton <keithc@isoftware.co.za>
2024-03-16 16:03:26 +0200

    Undo error of adding validation messages to the FormItem.

--- bank 5/lib/ui-toolkit/form_item.dart
+++ bank 5/lib/ui-toolkit/form_item.dart
@@ -5,10 +5,8 @@ class FormItem {
   final FormItemSource source;
   final String label;
   final bool required;
-  final String? requiredMessage;
   final int? maxLength;
-  final String? maxLengthMessage;
   final List<FormItemKeyValue>? valueList;
 
-  FormItem(this.key, this.source, this.label, this.required, [this.requiredMessage, this.maxLength, this.maxLengthMessage, this.valueList]);
+  FormItem(this.key, this.source, this.label, this.required, [this.maxLength, this.valueList]);
 }
