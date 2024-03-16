8cef1b822929ea23f4dce1b5f3eb40258ce05cb4
keithecza <keithc@isoftware.co.za>
2024-03-16 13:59:03 +0200

    Update bank 2/lib/model/bank.dart
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- bank 2/lib/model/bank.dart
+++ bank 2/lib/model/bank.dart
@@ -10,7 +10,7 @@ class Bank {
   factory Bank.fromJson(Map<String, dynamic> json) {
     List<Branch> branches = [];
     if (json["branches"] != null) {
-      (json["branches"] as List).map((branch) => Branch.fromJson(branch)).toList();
+      branches = (json["branches"] as List).map((branch) => Branch.fromJson(branch)).toList();
     }
     return Bank(json["name"], json["id"], branches);
   }
